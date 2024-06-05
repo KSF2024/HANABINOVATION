50090d193cf12082102d23916f6560044fdd5034
†村神† <CA01971172@st.kawahara.ac.jp>
2024-06-05 11:05:04 +0900

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import basicSsl from '@vitejs/plugin-basic-ssl';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+    base: "/HANABINOVATION/",
     server: {
         host: true,
     },
     plugins: [
         react(),
-        // basicSsl()
+        basicSsl()
     ]
 })
