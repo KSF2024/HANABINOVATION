ac12475968775ca85f64be39ce96b80093638c8e
†村神† <CA01971172@st.kawahara.ac.jp>
2024-06-05 10:13:00 +0900

    change the path of providers

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { CameraContext } from '../temp/CameraProvider';
+import { CameraContext } from '../providers/CameraProvider';
 
 // カメラの映像を表示するコンポーネント
 export default function Camera(){
