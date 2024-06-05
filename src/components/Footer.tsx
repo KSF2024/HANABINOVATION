2bf8eb250f777179f3f9632e5f2d09021614b08f
CA01971092 <107534446+CA01971092@users.noreply.github.com>
2024-06-05 13:13:03 +0900

    WIP LotteryEntryPage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import MapIcon from '@mui/icons-material/Map';
 import QrCode2Icon from '@mui/icons-material/QrCode2';
+import AdsClickIcon from '@mui/icons-material/AdsClick';
 import { styled } from '@mui/system'
 import HanabiIcon from '../images/hanabi.png'
 
@@ -11,18 +12,20 @@ const CustomBottomNavigation = styled(BottomNavigation)({
     backgroundColor: '#098FF0', // フッターの背景色
     color: 'white', // フッターの文字色
     borderRadius: '0',
-    width: '393px',
-    height: '63px'
+    weight: 393,
+    height: 63
   });
 
   const CustomBottomNavigationAction = styled(BottomNavigationAction)({
     color: 'white', // アイコンとラベルの色
+    weight: 393,
+    height: 63
   });
 
 const Footer = () => {
   const navigate = useNavigate();
 
-  const handleNavigation = (event, newValue) => {
+  const handleNavigation = (event, newValue:string) => {
     navigate(newValue);
   };
 
@@ -32,6 +35,7 @@ const Footer = () => {
       <CustomBottomNavigationAction label="花火大会" value="/hanabi"  icon={<img src={HanabiIcon} alt="Hanabi" style={{ width: 24, height: 24 }} />} /> 
       <CustomBottomNavigationAction label="マップ" value="/map" icon={<MapIcon />} />
       <CustomBottomNavigationAction label="QR読取" value="/qr" icon={<QrCode2Icon />} />
+      <CustomBottomNavigationAction label="抽選応募" value="/lottery" icon={<AdsClickIcon />} />
     </CustomBottomNavigation>
   );
 };
--- src/pages/LotteryPage.tsx
+++ src/pages/LotteryPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { FormControl } from '@mui/material';
 import '../style/LotteryPage.css';
 import { Icon } from '@mui/material';
 
@@ -10,77 +11,78 @@ const LotteryPage = () => {
   const [ Old, setOld] = useState('');
   const [ SchoolName, setSchoolName] = useState('');
   const [ Grade, setGrade] = useState('');
+  const [ Submited, setSubmited] = useState(false);
 
-  const handleUserNameChange = ( event ) =>{
-    setUserName(event.target.value)
-  }
+  function SubmitDialog(){
+    return <div id='Submitlog'>応募受付が完了しました。<div/>当選者の呼び出しはユーザー名と受付番号で行われます。</div>
+  };
 
-const  handleMailAddressChange= ( event ) =>{
-    setMailAddress(event.target.value)
-  }
-
-  const  handlePhoneNumberChange= ( event ) =>{
-    setPhoneNumber(event.target.value)
-  }
-
-  const  handleOldChange= ( event ) =>{
-    setOld(event.target.value)
-  }
-
-  const  handleSchoolNameChange= ( event ) =>{
-    setSchoolName(event.target.value)
-  }
-
-  const  handleGradeChange= ( event ) =>{
-    setGrade(event.target.value)
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmited(true);
+  };
 
   return <div>
-  
+  <form onSubmit={handleSubmit}>
   <div id='LotteryEntry'>
   抽選応募
-  <br/>
   </div>
     
     <div id='UserName'>
       ユーザー名(必須)
-      <br/>
-    <input value={UserName} onChange={handleUserNameChange} required />
+      <div/>
+    <input value={UserName} size={32} onChange={( event ) =>{
+    setUserName(event.target.value)
+  }} required />
     </div>
 
     <div id='MailAddress'>
       メールアドレス(必須)
-      <br/>
-      <input value={MailAddress} onChange={handleMailAddressChange} required />
+      <div/>
+      <input value={MailAddress} size={32} onChange={ ( event ) =>{
+    setMailAddress(event.target.value)
+  }} required />;
     </div>
 
     <div id='PhoneNumber'>
       電話番号(任意)
-      <br/>
-      <input value={PhoneNumber} onChange={handlePhoneNumberChange} />
+      <div/>
+      <input value={PhoneNumber} size={32} onChange={( event ) =>{
+    setPhoneNumber(event.target.value)
+  }} />;
     </div>
 
       <div id='Old'>
       年齢(任意)
-      <br/>
-      <input value={Old} onChange={handleOldChange} />
+      <div/>
+      <input value={Old} size={32} onChange={( event ) =>{
+    setOld(event.target.value)
+  }} />;
     </div>
 
     <div id='SchoolName'>
       学校名(任意)
-      <br/>
-      <input value={SchoolName} onChange={handleSchoolNameChange} />
+      <div/>
+      <input value={SchoolName} size={32} onChange={( event ) =>{
+    setSchoolName(event.target.value)
+  }} />
     </div>
 
     <div id='Grade'>
       学年(任意)
-      <br/>
-      <input value={Grade} onChange={handleGradeChange} />
+      <div/>
+      <input value={Grade} size={32} onChange={( event ) =>{
+    setGrade(event.target.value)
+  }} />;
     </div>
 
-  <input type='submit' value='応募' id='SubmitButton'/>
+  <input type='submit' value='応募' id='SubmitButton' onChange={(event) => {
+    event.preventDefault();
+    setSubmited(true);
+  }}/>
+  </form>
+  {Submited && <SubmitDialog />};
   </div>
 };
 
-  
 export default LotteryPage;
