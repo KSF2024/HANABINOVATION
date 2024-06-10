92c36ac26e780a4f7d155eca2f3c4ac694f66949
†村神† <CA01971172@st.kawahara.ac.jp>
2024-06-10 09:54:51 +0900

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import MapIcon from '@mui/icons-material/Map';
-import QrCode2Icon from '@mui/icons-material/QrCode2';
-import AdsClickIcon from '@mui/icons-material/AdsClick';
-import { styled } from '@mui/system'
-import HanabiIcon from '../images/hanabi.png'
-
-const CustomBottomNavigation = styled(BottomNavigation)({
-    backgroundColor: '#098FF0', // フッターの背景色
-    color: 'white', // フッターの文字色
-    borderRadius: '0',
-    weight: 393,
-    height: 63
-  });
-=======
 import { useNavigate } from "react-router-dom";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
@@ -25,7 +6,6 @@ import QrCode2Icon from "@mui/icons-material/QrCode2";
 import AdsClickIcon from "@mui/icons-material/AdsClick";
 import { styled } from "@mui/system"
 import HanabiIcon from "../images/hanabi.png"
->>>>>>> 2c5f83f (merge footer)
 
 const CustomBottomNavigationAction = styled(BottomNavigationAction)({
     color: "white" // アイコンとラベルの色
@@ -34,18 +14,6 @@ const CustomBottomNavigationAction = styled(BottomNavigationAction)({
 export default function Footer(){
     const navigate = useNavigate();
 
-<<<<<<< HEAD
-  return (
-    <CustomBottomNavigation onChange={handleNavigation} showLabels  >
-      <CustomBottomNavigationAction label="再撮影" value="/photo" icon={<CameraAltIcon />} />
-      <CustomBottomNavigationAction label="花火大会" value="/hanabi"  icon={<img src={HanabiIcon} alt="Hanabi" style={{ width: 24, height: 24 }} />} /> 
-      <CustomBottomNavigationAction label="マップ" value="/map" icon={<MapIcon />} />
-      <CustomBottomNavigationAction label="QR読取" value="/qr" icon={<QrCode2Icon />} />
-      <CustomBottomNavigationAction label="抽選応募" value="/lottery" icon={<AdsClickIcon />} />
-    </CustomBottomNavigation>
-  );
-};
-=======
     return (
         <BottomNavigation
             showLabels
@@ -90,4 +58,3 @@ export default function Footer(){
         </BottomNavigation>
     )
 }
->>>>>>> 2c5f83f (merge footer)
