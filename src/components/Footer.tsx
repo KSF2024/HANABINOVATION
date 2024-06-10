0bbc26ed9c9e28b7891a893585c125992dda3485
†村神† <CA01971172@st.kawahara.ac.jp>
2024-06-10 09:46:42 +0900

    merge footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
@@ -15,6 +16,16 @@ const CustomBottomNavigation = styled(BottomNavigation)({
     weight: 393,
     height: 63
   });
+=======
+import { useNavigate } from "react-router-dom";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import MapIcon from "@mui/icons-material/Map";
+import QrCode2Icon from "@mui/icons-material/QrCode2";
+import AdsClickIcon from "@mui/icons-material/AdsClick";
+import { styled } from "@mui/system"
+import HanabiIcon from "../images/hanabi.png"
+>>>>>>> 2c5f83f (merge footer)
 
 const CustomBottomNavigationAction = styled(BottomNavigationAction)({
     color: "white" // アイコンとラベルの色
@@ -23,6 +34,7 @@ const CustomBottomNavigationAction = styled(BottomNavigationAction)({
 export default function Footer(){
     const navigate = useNavigate();
 
+<<<<<<< HEAD
   return (
     <CustomBottomNavigation onChange={handleNavigation} showLabels  >
       <CustomBottomNavigationAction label="再撮影" value="/photo" icon={<CameraAltIcon />} />
@@ -33,3 +45,49 @@ export default function Footer(){
     </CustomBottomNavigation>
   );
 };
+=======
+    return (
+        <BottomNavigation
+            showLabels
+            onChange={(_event: React.SyntheticEvent<Element, Event>, value: string) => {
+                // 指定パスにURL遷移を行う
+                navigate(value);
+            }}
+            sx={{
+                backgroundColor: "#098FF0" // フッターの背景色
+            }}
+            style={{
+                height: "4rem"
+            }}
+        >
+            <CustomBottomNavigationAction
+                value="/001/capture-firework" // TODO ブースIDの指定
+                label="再撮影"
+                icon={<CameraAltIcon/>}
+            />
+            <CustomBottomNavigationAction
+                value="/firework-show"
+                label="花火大会"
+                icon={<img src={HanabiIcon} alt="Hanabi" style={{ width: 24, height: 24 }}/>}
+            /> 
+            <CustomBottomNavigationAction
+                value="/map?booth=001" // TODO ブースIDの指定
+                label="マップ"
+                icon={<MapIcon/>}
+            />
+            <CustomBottomNavigationAction
+                value="/scan-qr"
+                label="QR読取"
+                icon={<QrCode2Icon/>}
+            />
+            {false && (
+                <CustomBottomNavigationAction
+                    value="/enter-lottery"
+                    label="抽選応募"
+                    icon={<AdsClickIcon/>}
+                />
+            )}
+        </BottomNavigation>
+    )
+}
+>>>>>>> 2c5f83f (merge footer)
