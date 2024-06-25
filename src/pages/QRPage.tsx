e04d108fe5d0eaf9cef5bb10880ac7a0a8e6ff16
CA01971092 <107534446+CA01971092@users.noreply.github.com>
2024-06-25 11:15:42 +0900

    WIP QRPage

--- src/pages/QRPage.tsx
+++ src/pages/QRPage.tsx
@@ -1,5 +1,71 @@
+import Camera from "../components/Camera";
+import { Box } from "@mui/material";
+import QRCodeReader from "../images/QRCodeReader.png"
+import QRScanner from "react-qr-scanner";
+import { useState } from "react";
+import FooterPage from "../components/FooterPage";
+
 export default function QRPage(){
-    return (
-        <div>QRコード</div>
+
+    const [result, setResult] = useState<string>('');
+
+    const handleScan = (data: any) => {
+      if (data) {
+        setResult(data.text);
+      }
+    };
+  
+    const handleError = (err: any) => {
+      console.error(err);
+    };
+
+    return(
+        <>
+            <FooterPage>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        width: "100%",
+                        height: "100vh",
+                        textAlign: "center",
+                        position: "absolute",
+                        zIndex: "1"
+                    }}
+                >
+                    <QRScanner
+                        delay={300}
+                        onError={handleError}
+                        onScan={handleScan}
+                        style={{ width: '100%', height: "100vh" }}
+                    />
+                    {result && (
+                        <Box sx={{ marginTop: 2 }}>
+                            <p>スキャン結果: {result}</p>
+                        </Box>
+                    )}
+                </Box>
+                <Box sx={{
+                        width: "100%",
+                        height: "100vh",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        textAlign: "center",
+                        position: "absolute",
+                        zIndex: "2"
+                    }}
+                >
+                    <img 
+                        src={QRCodeReader}
+                        alt="qrcode_reader"
+                        style={{height: "20rem", paddingBottom: "5rem"}}
+                    >
+                    </img>
+                </Box>
+            </FooterPage>
+        </>
     )
 }
