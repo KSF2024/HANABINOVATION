669d839b6d21bd58be35e41f850d651790ed42fb
†村神† <107534447+CA01971172@users.noreply.github.com>
2024-07-04 21:26:04 +0900

    Prepare the canvas element

--- src/pages/FireworksDisplayPage.tsx
+++ src/pages/FireworksDisplayPage.tsx
@@ -8,12 +8,23 @@ export default function FireworksDisplayPage(){
                 src={NightSky}
                 style={window.innerWidth > 960 ? {
                     width: "100%",
-                    height: "100%",
+                    height: "100%"
                 } : {
                     width: "100%",
                     height: "100%",
                     objectFit: "cover",
                     objectPosition: "center"
+                }}
+            />
+            <canvas
+                style={{
+                    zIndex: 10,
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    width: "100%",
+                    height: "100%",
+                    maxHeight: "calc(100dvh - 4rem)"
                 }}
             />
         </FooterPage>
