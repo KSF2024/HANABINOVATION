b7e7ce2c6e158334e922784a31b99892a1283c9b
†村神† <107534447+CA01971172@users.noreply.github.com>
2024-07-04 15:56:20 +0900

    Implement the background

--- src/pages/FireworksDisplayPage.tsx
+++ src/pages/FireworksDisplayPage.tsx
@@ -1,9 +1,21 @@
 import FooterPage from "../components/FooterPage";
+import NightSky from "../images/night_sky.png";
 
 export default function FireworksDisplayPage(){
     return (
         <FooterPage>
-            <div>花火大会</div>
+            <img
+                src={NightSky}
+                style={window.innerWidth > 960 ? {
+                    width: "100%",
+                    height: "100%",
+                } : {
+                    width: '100%',
+                    height: "100%",
+                    objectFit: 'cover',
+                    objectPosition: 'center',
+                }}
+            />
         </FooterPage>
     )
 }
