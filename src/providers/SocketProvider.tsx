6cc2d8aa1eca033c025763f543efc0a6797d866d
†村神† <107534447+CA01971172@users.noreply.github.com>
2024-07-07 20:33:32 +0900

    Implement pushFireworksData

---
+++ src/providers/SocketProvider.tsx
@@ -0,0 +1,125 @@
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
+import { WS_ENDPOINT } from '../utils/apiClient';
+import { MultiFireworksContext } from './MultiFireworksProvider';
+
+/* 型定義 */
+// contextに渡すデータの型
+type Context = {
+    socketRef: React.MutableRefObject<WebSocket | null>;
+};
+
+
+/* Provider */
+const initialData: Context = {
+    socketRef: {} as React.MutableRefObject<WebSocket | null>
+};
+
+export const SocketContext = createContext<Context>(initialData);
+
+// websocketを管理するプロバイダー
+export function SocketProvider({children}: {children: ReactNode}){
+    /* state, context */
+    const socketRef = useRef<WebSocket | null>(null);
+    const {
+        pageMode,
+        animateFirework
+    } = useContext(MultiFireworksContext);
+
+    /* useEffect */
+    // WebSocket関連の処理は副作用なので、useEffect内で実装
+    useEffect(() => {
+        // WebSocketオブジェクトを生成しサーバとの接続を開始
+        let websocket: WebSocket = new WebSocket(WS_ENDPOINT);
+        console.log("websocket:", websocket);
+        socketRef.current = websocket;
+
+        // メッセージ受信時のイベントハンドラ関数
+        // そのままだとreactで管理している状態を取得できないので、useState + useEffectを経由させる
+        function onMessage(event: MessageEvent<any>){
+            setWsEvent(event);
+        }
+
+        // websocket接続切断時のイベントハンドラ関数
+        function onClose(){
+            console.log("websocket接続がタイムアウトしました");
+            // showErrorToast("E008"); //「サーバーとの接続が切断されました。」
+
+            // websocket切断時、websocketに再接続する
+            socketRef.current = new WebSocket(WS_ENDPOINT);
+            socketRef.current.addEventListener("message", onMessage);
+            socketRef.current.addEventListener("close", onClose);
+            console.log("websocketに再び接続しました");
+        }
+
+        // websocketインスタンスにイベントハンドラを登録する
+        websocket.addEventListener("message", onMessage);
+        websocket.addEventListener("close", onClose);
+
+        // useEffectのクリーンアップの中で、WebSocketのクローズ処理を実行
+        return () => {
+            websocket.close();
+            websocket.removeEventListener('message', onMessage);
+            console.log("websocket接続が切れました");
+        }
+    }, [])
+
+    // websocketのeventを監視する
+    // addEventListenerを設定したタイミングの状態しか取得できないようなので、useEffect経由で状態を無理矢理取得する
+    const [wsEvent, setWsEvent] = useState<MessageEvent<any> | null>(null);
+    useEffect(() => {
+        if(wsEvent) handleWsEvent(wsEvent);
+    }, [wsEvent]);
+
+
+    /* function */
+    // メッセージ受信時のイベントハンドラ関数
+    function handleWsEvent(event: MessageEvent<any>){
+        console.log("wsOnMessage:", {event});
+
+        // 受信したメッセージデータを処理する
+        try{
+            // 受け取ったレスポンスデータを取得する
+            const response: any = JSON.parse(event.data);
+            console.log("received data:", response);
+
+            // actionに応じた処理を行う
+            switch(response.action){
+                case "show-firework":
+                    // 撮影された花火を、花火大会画面に打ち上げる
+                    showFirework(response.data);
+                    break;
+                case "draw-lottery":
+                    break;
+                case "send-fireworks":
+                    break;
+                default:
+                    break;
+            }
+
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    // 現在開いているのが花火大会画面なら、花火撮影機能で撮影された花火を即時画面上に打ち上げる関数
+    function showFirework(data: any){
+        if(pageMode !== "show-fireworks") return;
+        console.log("show firework")
+        const boothId: string = data.boothId;
+        const fireworkType: number = data.fireworkType;
+        const fireworkDesign: Blob | null = data.fireworkDesign || null;
+        const sparksType: number = data.sparksType;
+        animateFirework(boothId, fireworkType, fireworkDesign, sparksType);
+    }
+
+
+    return (
+        <SocketContext.Provider
+            value={{
+                socketRef
+            }}
+        >
+            {children}
+        </SocketContext.Provider>
+    );
+}
--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -3,7 +3,7 @@ import { FireworkData, FireworksData, HoleFireworksData, Profile, Registration }
 
 // API呼び出し用のURLを定義する
 const API_ENDPOINT: string = "https://vfml5unckb.execute-api.ap-northeast-1.amazonaws.com/dev/api/v1";
-const WS_ENDPOINT: string = "wss://pcg2x1k5lj.execute-api.ap-northeast-1.amazonaws.com/dev/";
+export const WS_ENDPOINT: string = "wss://pcg2x1k5lj.execute-api.ap-northeast-1.amazonaws.com/dev/";
 // const API_ENDPOINT: string = "https://hanabinovation.org/api/v1";
 // const WS_ENDPOINT: string = "wss://hanabinovation.org/api/v1/websocket";
 
