57ed3d6fbb13ad9fb6f0a7d20811bf9dbdd4dca4
†村神† <107534447+CA01971172@users.noreply.github.com>
2024-07-09 20:13:16 +0900

    Fix camera facing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
     server: {
@@ -8,6 +8,6 @@ export default defineConfig({
     },
     plugins: [
         react(),
-        // basicSsl()
+        basicSsl()
     ]
 })
