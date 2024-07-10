b6c8d8a2e070c8209688e98a9da7b13336007984
CA01971092 <107534446+CA01971092@users.noreply.github.com>
2024-07-10 16:40:28 +0900

    wip MapPage

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,25 +1,81 @@
+import { CSSProperties, useContext } from "react";
 import FooterPage from "../components/FooterPage";
-import mapbooth from "../images/mapbooth.png"
+import mapbooth from "../images/map_booth.png";
+import { SCHOOL_DATA } from "../utils/config";
+import { DataContext } from "../providers/DataProvider";
 
 export default function MapPage(){
+
+    const schoolEntries = Object.entries(SCHOOL_DATA)
+
+    const { postedFireworksData } = useContext(DataContext);
+
+    const getPinStyle = ( positionPinX:number, positionPinY:number ): CSSProperties =>({
+        position: "absolute" as "absolute",
+        top: `${positionPinY}vh`,
+        left: `${positionPinX}vw`,
+        transform: "translate(-50%, -50%)",
+        width: "15vw",
+        filter: "drop-shadow(5px 5px 0px rgba(0, 0, 0, 0.5))",
+        animation: 'floatUpDown 2s ease-in-out infinite',
+    })
+
+    useEffect(() => {
+        if(!postedFireworksData) return;
+        const postedBoothIdList: string[] = Object.keys(postedFireworksData);
+    }, [postedFireworksData ]);
+
     return (
         <FooterPage>
-            <div 
-                style={{
-                    maxHeight: "100%",
-                    width: "100%",
-                    height: "100%",
-                    position: "relative",
-                    display: "flex",
-                    justifyContent: "center",
-                    textAlign: "center"
-                }}
-            >
-                <img 
-                    src={mapbooth}
-                    style={{maxWidth: "100%", maxHeight: "100%"}}
+            <div>
+                <div 
+                    style={{
+                        maxHeight: "100%",
+                        width: "100%",
+                        height: "100%",
+                        position: "relative",
+                        display: "flex",
+                        justifyContent: "center",
+                        textAlign: "center"
+                    }}
                 >
-                </img>
+                    <img 
+                        src={mapbooth}
+                        style={{width: "100%", height: "96vh"}}
+                    >
+                    </img>
+                </div>
+                <div>
+                    {schoolEntries.map(([schoolName,schoolInfo], index) => (
+                        <div>
+                            <img
+                                key={index} 
+                                src={`${schoolInfo.mapPin}`}
+                                style={
+                                    getPinStyle(
+                                        schoolInfo.positionPinX,
+                                        schoolInfo.positionPinY
+                                    )
+                                }
+                            />
+                            <div
+                                style={{
+                                    position: "absolute",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    top: `${schoolInfo.schoolNameY}vh`,
+                                    left: `${schoolInfo.schoolNameX}vw`,
+                                    width: `${schoolInfo.schoolNameWidth}vw`,
+                                    fontSize: "3.5vw",
+                                    color: "#FFFFFF"
+                                }}
+                            >
+                                {schoolInfo.schoolName}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </FooterPage>
     )
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -28,14 +28,27 @@ import image9_3 from "./../images/松山デザイナー専門学校/3.png";
 import image10_1 from "./../images/河原医療大学校 新居浜校/1.png";
 import image10_2 from "./../images/河原医療大学校 新居浜校/2.png";
 import image10_3 from "./../images/河原医療大学校 新居浜校/3.png";
+import pin_image_denshi from "./../images/マップピン/河原電子ビジネスマップピン.png";
+import pin_image_gaigoseika from "./../images/マップピン/河原外語・製菓マップピン.png";
+import pin_image_ooharaboki from "./../images/マップピン/大原簿記公務員マップピン.png";
+import pin_image_aipetto from "./../images/マップピン/河原アイペットワールドマップピン.png";
+import pin_image_iryou from "./../images/マップピン/河原医療大学校マップピン.png";
+import pin_image_iryouhukushi from "./../images/マップピン/河原医療福祉マップピン.png";
+import pin_image_beauti from "./../images/マップピン/河原ビューティ.png";
+import pin_image_design from "./../images/マップピン/河原デザイン・アートマップピン.png";
+import pin_image_matsuyamadesigner from "./../images/マップピン/松山デザイナーマップピン.png";
+import pin_image_iryou_niihama from "./../images/マップピン/河原医療大学校新居浜校マップピン.png"
 
 type SchoolInfo = {
     schoolName: string; // 学校名
     color: string; // 学校のテーマカラー(カラーコード)
     fireworksImages: {0: string, 1: string, 2: string}; // 花火の画像
-    positionX: number; // マップ上のブースの位置(横軸)
-    positionY: number; // マップ上のブースの位置(縦軸)
-    MapData: MapInfo;
+    positionPinX: number; //マップのピン横軸 単位は%
+    positionPinY: number; //マップのピン縦軸 単位は%
+    schoolNameX: number; //表示される学校名の縦軸 単位は%
+    schoolNameY: number; //表示される学校名の横軸 単位は%
+    schoolNameWidth: number; //表示される学校名の文字サイズ 単位はvw
+    mapPin: string;
 }
 
 type OpencampusDate = {
@@ -43,85 +56,117 @@ type OpencampusDate = {
     date: string; //オープンキャンパスの日付
 }[];
 
-type MapInfo = {
-    positionPinX: number;
-    positionPinY: number;
-    schoolNameX: number;
-    schoolNameY: number;
-    schoolNameWidth: number;
-}
-
 // 各学校の設定データ
 export const SCHOOL_DATA: { [boothId: string]: SchoolInfo } = {
     "HF5W2T": {
         schoolName: "河原電子ビジネス専門学校",
         color: "#00FFFF",
         fireworksImages: [image1_1, image1_2, image1_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 29,
+        positionPinY: 26,
+        schoolNameX: 12,
+        schoolNameY: 29.5,
+        schoolNameWidth: 52,
+        mapPin: pin_image_denshi
     },
     "Y6XBJH": {
         schoolName: "河原外語観光・製菓専門学校",
         color: "#4800FF",
         fireworksImages: [image2_1, image2_2, image2_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 5,
+        positionPinY: 0,
+        schoolNameX: 0,
+        schoolNameY: 0,
+        schoolNameWidth: 0,
+        mapPin: pin_image_gaigoseika
     },
     "7JDZVP": {
         schoolName: "大原簿記公務員専門学校 愛媛校",
         color: "#FF0000",
         fireworksImages: [image3_1, image3_2, image3_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 10,
+        positionPinY: 0,
+        schoolNameX: 0,
+        schoolNameY: 0,
+        schoolNameWidth: 0,
+        mapPin: pin_image_ooharaboki
     },
     "SHSQ4A": {
         schoolName: "河原アイペットワールド専門学校",
         color: "#FFA500",
         fireworksImages: [image4_1, image4_2, image4_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 5,
+        positionPinY: 0,
+        schoolNameX: 0,
+        schoolNameY: 0,
+        schoolNameWidth: 0,
+        mapPin: pin_image_aipetto
     },
     "FZVSW0": {
         schoolName: "河原医療大学校",
         color: "#00FF00",
         fireworksImages: [image5_1, image5_2, image5_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 50,
+        positionPinY: 31.5,
+        schoolNameX: 25,
+        schoolNameY: 35.5,
+        schoolNameWidth: 30,
+        mapPin: pin_image_iryou
     },
     "WA067Z": {
         schoolName: "河原医療福祉専門学校",
         color: "#CDFF00",
         fireworksImages: [image6_1, image6_2, image6_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 5,
+        positionPinY: 0,
+        schoolNameX: 0,
+        schoolNameY: 0,
+        schoolNameWidth: 0,
+        mapPin: pin_image_iryouhukushi
     },
     "94VPFZ": {
         schoolName: "河原ビューティモード専門学校",
         color: "#FF00FF",
         fireworksImages: [image7_1, image7_2, image7_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 5,
+        positionPinY: 0,
+        schoolNameX: 0,
+        schoolNameY: 0,
+        schoolNameWidth: 0,
+        mapPin: pin_image_beauti
     },
     "5HGS6W": {
         schoolName: "河原デザイン・アート専門学校",
         color: "#FFFF00",
         fireworksImages: [image8_1, image8_2, image8_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 5,
+        positionPinY: 0,
+        schoolNameX: 0,
+        schoolNameY: 0,
+        schoolNameWidth: 0,
+        mapPin: pin_image_design
     },
     "HDE5W4": {
         schoolName: "松山デザイナー専門学校",
         color: "#00FF00",
         fireworksImages: [image9_1, image9_2, image9_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 5,
+        positionPinY: 0,
+        schoolNameX: 0,
+        schoolNameY: 0,
+        schoolNameWidth: 0,
+        mapPin: pin_image_matsuyamadesigner
     },
     "Y9KFFH": {
         schoolName: "河原医療大学校 新居浜校",
         color: "#00FFAC",
         fireworksImages: [image10_1, image10_2, image10_3],
-        positionX: 0,
-        positionY: 0
+        positionPinX: 5,
+        positionPinY: 0,
+        schoolNameX: 0,
+        schoolNameY: 0,
+        schoolNameWidth: 0,
+        mapPin: pin_image_iryou_niihama
     }
 };
 
@@ -172,63 +217,3 @@ export const OPENCAMPUS_DATE: OpencampusDate[] = [
         {url: "https://kbc.kawahara.ac.jp/oc/2024-06-22/", date: "6/22"}
     ]
 ];
-
-export const mapData: {[schoolName:string]:MapInfo} = {
-    "kawaharaDenshi": {
-        positionPinX: 0,
-        positionPinY: 0,
-        schoolNameX: 0,
-        schoolNameY: 0,
-        schoolNameWidth: 0
-    },
-    "kawaharaGaigoKankouSeika": {
-        positionPinX: 0,
-        positionPinY: 0,
-        schoolNameX: 0,
-        schoolNameY: 0,
-        schoolNameWidth: 0
-    },
-    "kawaharaIryou": {
-        positionPinX: 0,
-        positionPinY: 0,
-        schoolNameX: 0,
-        schoolNameY: 0,
-        schoolNameWidth: 0
-    },
-    "kawaharaIryouHukushi": {
-        positionPinX: 0,
-        positionPinY: 0,
-        schoolNameX: 0,
-        schoolNameY: 0,
-        schoolNameWidth: 0
-    },
-    "kawaharaBeauty": {
-        positionPinX: 0,
-        positionPinY: 0,
-        schoolNameX: 0,
-        schoolNameY: 0,
-        schoolNameWidth: 0
-    },
-    "kawaharaDesign": {
-        positionPinX: 0,
-        positionPinY: 0,
-        schoolNameX: 0,
-        schoolNameY: 0,
-        schoolNameWidth: 0
-    },
-    "matsuyamaDesigner": {
-        positionPinX: 0,
-        positionPinY: 0,
-        schoolNameX: 0,
-        schoolNameY: 0,
-        schoolNameWidth: 0
-    },
-    "kawaharaIryouNiihama": {
-        positionPinX: 0,
-        positionPinY: 0,
-        schoolNameX: 0,
-        schoolNameY: 0,
-        schoolNameWidth: 0
-    }
-}
-
